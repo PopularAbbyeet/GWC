bc736e123652da74d48b8ef7e1c7690e90a48b25
PopularAbbyeet <abbyeatsgames@gmail.com>
2025-08-06 14:09:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -55,29 +55,27 @@ function draw() {
    			fullscreen(!fs);
     		}
   	}
-	if (screen === 1) {
-		if (frameCount % 100 == 0) {
-			new Sprite(random(canvas.w), 0, 30, 30);
-		}
-
-	if (mouse.presses()) {
-		let s = world.getSpriteAt(mouse);
-		if (s) {
-			j = new GrabberJoint(s);
-			j.maxForce = 1000;
-		}
-	}
-
-	if (mouse.pressing() && j) j.target = mouse;
-	if (mouse.released() && j) j.remove();
-
-  	}
 }
 /* FUNCTIONS TO DISPLAY SCREENS */
 function showScreen1() {
   background("lightblue");
-  text("Screen 1 work", width / 2, height / 2 - 100);
   startButton.pos = { x: -100, y: -100 };
+  if (screen === 1) {
+	  if (frameCount % 100 == 0) {
+		  new Sprite(random(canvas.w), 0, 30, 30);
+	  }
+	  if (mouse.presses()) {
+		  let s = world.getSpriteAt(mouse);
+		  if (s) {
+			  j = new GrabberJoint(s);
+			  j.maxForce = 1000;
+		  }
+	  }
+
+	  if (mouse.pressing() && j) j.target = mouse;
+	  if (mouse.released() && j) j.remove();
+
+  	  }
 }
 
 
